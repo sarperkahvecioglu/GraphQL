eb3ccba456112ad91801258d1fa53a26653b6716
Sarper Kahvecioğlu <112126303+sarperkahvecioglu@users.noreply.github.com>
2023-08-15 15:55:58 +0300

    Add files via upload

---
+++ GraphQL-WorkingWithRelationalData/data.js
@@ -0,0 +1,41 @@
+export const authors = [
+    {
+      id: "001",
+      name: "Alexis",
+      surname: "Jordan",
+      age: 22,
+      
+    },
+    {
+      id: "002",
+      name: "Andrew",
+      surname: "James",
+      age: 21,
+      
+    },
+  ];
+  
+  export const books = [
+    {
+      title: "Life Passed",
+      author_id: "001",
+      id: "1",
+      score: 6.9,
+      isPublished: true,
+    },
+    {
+      title: "Mom",
+      author_id: "002",
+      id: "2",
+      score: 7.4,
+      isPublished: true,
+    },
+    {
+      title: "Ford Improvement",
+      author_id: "002",
+      id: "3",
+      score: 8.4,
+      isPublished: true,
+    },
+  ];
+  
---
+++ GraphQL-WorkingWithRelationalData/index.js
@@ -0,0 +1,79 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { books, authors } from "./data.js";
+
+
+
+const typeDefs = gql`
+  type Author {
+    id: ID!
+    name: String!
+    surname: String
+    age: Int!
+    books: [Book!]
+  }
+
+  type Book {
+    title: String!
+    author: Author!
+    author_id: String!
+    id: ID!
+    score: Float
+    isPublished: Boolean
+  }
+
+
+
+  type Query {
+    books: [Book!]
+    book(id: ID!): Book!
+    authors: [Author!]
+    author(id: ID!): Author!
+    
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    authors: () => authors,
+    book: (_, args) => {
+      
+      const data = books.find(book => book.id === args.id);
+      return data;
+    },
+
+    author: (_, args) => {
+      const data = authors.find(author => author.id === args.id);
+
+      return data;
+    },
+  },
+
+  Book: {
+    author: (parent) => {
+      const data = authors.find(author => author.id === parent.author_id)
+      
+      return data;
+    }
+  },
+
+  Author: {
+    books: (parent) => {
+      const data = books.filter(book => book.author_id === parent.id);
+
+      return data;
+    }
+  }
+
+
+
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => console.log(`Apollo server is up at ${url}`));
